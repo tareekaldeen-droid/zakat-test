a425ca03787736d44549d97c5c52224fe8502e92
tareekaldeen-droid <tareekaldeen@gmail.com>
2025-12-28 13:06:37 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'zakat-app-v 4.2';
+const CACHE_NAME = 'zakat-app-v 4.3';
 // الملفات التي سيتم تخزينها مؤقتاً (CSS, JS, Images)
 const ASSETS_TO_CACHE = [
   './',
@@ -57,6 +57,7 @@ self.addEventListener('fetch', (event) => {
     );
   }
 });
+
 
 
 
